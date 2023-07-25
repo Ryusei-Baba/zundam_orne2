45596f2e0fa8b2f84c8079c1db69e01bdd7a7ed9
Ryusei Baba <s20c1102kg@s.chibakoudai.jp>
2023-07-25 20:34:12 +0900

    add self.flg and sleep

--- zundam_orne2/zundam_orne2_node.py
+++ zundam_orne2/zundam_orne2_node.py
@@ -15,35 +15,41 @@ class ZundamSubscriber(Node):
         super().__init__('zundam_subscriber_node') 
         self.subscription = self.create_subscription(Twist,'cmd_vel', self.callback, 10)
         self.vel = Twist()
+        self.flg = 0
                
     def callback(self, Twist): 
-        if Twist.linear.x == 0.0:
-            playsound(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../src/zundam_orne2/voice/004_ずんだもん（ノーマル）_どいてニャーーーー….wav"))
-            return Twist
-        elif Twist.linear.x > 0.2 and Twist.angular.z < 0.2 and Twist.angular.z > -0.2:
-            playsound(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../src/zundam_orne2/voice/001_ずんだもん（ノーマル）_まっすぐ進むのだ.wav"))
-            return Twist
-        elif Twist.angular.z < -0.5:
-            playsound(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../src/zundam_orne2/voice/002_ずんだもん（ノーマル）_右に曲がるのだ.wav"))
-            return Twist
-        elif Twist.angular.z > 0.5:
-            playsound(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../src/zundam_orne2/voice/003_ずんだもん（ノーマル）_左に曲がるのだ.wav"))
-            return Twist
-        else:
+        if self.flg == 0:
+            if Twist.linear.x == 0.0:
+                playsound(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../src/zundam_orne2/voice/004_ずんだもん（ノーマル）_どいてニャーーーー….wav"))
+                self.flg = 1
+                return Twist
+            elif Twist.linear.x > 0.2 and Twist.angular.z < 0.2 and Twist.angular.z > -0.2:
+                playsound(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../src/zundam_orne2/voice/001_ずんだもん（ノーマル）_まっすぐ進むのだ.wav"))
+                self.flg = 1
+                return Twist
+            elif Twist.angular.z <= -0.2:
+                playsound(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../src/zundam_orne2/voice/002_ずんだもん（ノーマル）_右に曲がるのだ.wav"))
+                self.flg = 1
+                return Twist
+            elif Twist.angular.z >= 0.2:
+                playsound(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../src/zundam_orne2/voice/003_ずんだもん（ノーマル）_左に曲がるのだ.wav"))
+                self.flg = 1
+                return Twist
+            else:
+                return Twist
+        elif self.flg == 1:
+            time.sleep(5)    # default5秒
+            self.flg = 0
             return Twist
 
-    
-        # self.get_logger().info("Velocity: Linear=%f angular=%f" % (Twist.linear.x,Twist.angular.z)) 
-   
    
 def main():
     rclpy.init()
     node = ZundamSubscriber()
     try:
-        rclpy.spin_once(node) 
+        rclpy.spin(node) 
     except KeyboardInterrupt:
         print("Ctrl+C が押されました．")
     finally:
         rclpy.shutdown()
         print('プログラム終了')
-
